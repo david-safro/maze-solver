b9e58efa31e89560a54a84d16ba28ee3ab678c75
Glitchez <87347098+Glitchez-1984@users.noreply.github.com>
2023-05-26 19:05:40 -0400

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,15 @@
+# queue.py
+#working
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
